b5d908a9d87b4964138888c9cf45134699363724
Emily M <30880308+e0marina@users.noreply.github.com>
2020-02-26 21:33:39 -0800

    created concert function using bandsintown api

--- liri.js
+++ liri.js
@@ -7,18 +7,28 @@ const axios = require("axios");
 
 const spotify = new Spotify(keys.spotify);
 
-//artist will be user input
-let artist = process.argv[2] + process.argv[3] + process.argv[4];
+let action = process.argv[2];
+//switch statement
+switch (action) {
+  case "concert-this":
+    concert();
+    break;
+}
 
-axios
+//function for getting concert info
+function concert() {
+  //artist will be user input
+  let artist = process.argv[3] + process.argv[4] + process.argv[5]; // this doesn't work if name is shorter or longer...figure out how to handle input of more than one word
+  axios
 
-  .get(
-    "https://rest.bandsintown.com/artists/" +
-      artist +
-      "/events?app_id=codingbootcamp"
-  )
-  .then(resp => {
-    console.log(artist);
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        artist +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(resp => {
+      console.log(artist);
 
-    console.log(resp.data); //will come back as an empty array if there are no shows coming up for the artist
-  });
+      console.log(resp.data); //will come back as an empty array if there are no shows coming up for the artist
+    });
+}
