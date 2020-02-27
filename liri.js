8b199a0ee21d68a079f831ac7e1c618bda256e6f
Emily M <30880308+e0marina@users.noreply.github.com>
2020-02-26 20:10:31 -0800

    installed spotifys node package

--- liri.js
+++ liri.js
@@ -1,6 +1,18 @@
-js;
 require("dotenv").config();
-js;
+
 var keys = require("./keys.js");
-js;
+var Spotify = require("node-spotify-api");
+
 var spotify = new Spotify(keys.spotify);
+//testing out if I can get stuff via Axios
+console.log(spotify);
+
+const axios = require("axios");
+
+axios
+  .get(
+    "https://rest.bandsintown.com/artists/chance+the+rapper/events?app_id=codingbootcamp"
+  )
+  .then(resp => {
+    console.log(resp.data);
+  });
