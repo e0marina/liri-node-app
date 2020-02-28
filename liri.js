3c28cf902ddb66131308ade11344123c203bdaf2
Emily M <30880308+e0marina@users.noreply.github.com>
2020-02-27 17:23:07 -0800

    moved for loop for userInput inside the function for concert-this

--- liri.js
+++ liri.js
@@ -15,37 +15,34 @@ switch (action) {
     break;
 }
 
-//user will be inputting more than one word for artists, movies, etc., so we need to push the process.argvs into an array
-// Store all of the arguments in an array
-var nodeArgs = process.argv;
-console.log(nodeArgs);
-
-// Create an empty variable for holding users input
-var userInput = "";
-console.log(userInput);
-
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 3; i < nodeArgs.length; i++) {
-  if (i > 3 && i < nodeArgs.length) {
-    userInput = userInput + "+" + nodeArgs[i];
-  } else {
-    userInput += nodeArgs[i];
-  }
-}
-
 //function for getting concert info
 function concert() {
-  axios
+  //user will be inputting more than one word for artists, movies, etc., so we need to push the process.argvs into an array
+  // Store all of the arguments in an array
+  var nodeArgs = process.argv;
+  // console.log(nodeArgs);
 
+  // Create an empty variable for holding users input
+  var userInput = "";
+  console.log(userInput);
+
+  // Loop through all the words in the node argument
+  // And do a little for-loop magic to handle the inclusion of "+"s
+  for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      userInput = userInput + "+" + nodeArgs[i];
+    } else {
+      userInput += nodeArgs[i];
+    }
+  }
+  axios
     .get(
       "https://rest.bandsintown.com/artists/" +
         userInput +
         "/events?app_id=codingbootcamp"
+      // "https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp"
     )
     .then(resp => {
-      console.log(userInput);
-
       console.log(resp.data); //will come back as an empty array if there are no shows coming up for the artist
       if (resp.data === []) {
         console.log(
