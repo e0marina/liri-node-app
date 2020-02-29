083e703b5e37b26e86d68a48c8b32efa1af661ac
Emily M <30880308+e0marina@users.noreply.github.com>
2020-02-28 20:21:18 -0800

    able to get the spotify api to respond

--- liri.js
+++ liri.js
@@ -11,10 +11,14 @@ const moment = require("moment");
 const spotify = new Spotify(keys.spotify);
 
 let action = process.argv[2];
+
 //switch statement
 switch (action) {
   case "concert-this":
     concert();
+    break;
+  case "spotify-this-song":
+    spotifyFunc();
     break;
 }
 
@@ -60,5 +64,33 @@ function concert() {
           "please search for another artist, this one doesn't have shows coming up"
         );
       }
+    });
+}
+//function for spotify portion of the app
+function spotifyFunc() {
+  var nodeArgs = process.argv;
+  // console.log(nodeArgs);
+
+  // Create an empty variable for holding users input
+  var userInput = "";
+  // console.log(userInput);
+
+  // Loop through all the words in the node argument
+  // And do a little for-loop magic to handle the inclusion of "+"s
+  for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      userInput = userInput + "+" + nodeArgs[i];
+    } else {
+      userInput += nodeArgs[i];
+    }
+  }
+
+  spotify
+    .search({ type: "track", query: userInput })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
